dd35dd096b2cbf12dbeef289edd59a7c91a10fc4
Franklin Fuchs <fuchs.franklin@gmail.com>
2020-08-03 15:13:57 -0700

    Minor grammar and formatting corrections to all sections.

--- franklinfuchs.R
+++ franklinfuchs.R
@@ -32,13 +32,13 @@ ui <- tagList(
                           img(src="unr_logo.png", height = '50px', width = '50px',align="top"),
                           
                           h4("Professional Bio"),
-                          p("Currently, I am an honors student majoring in mathematics and minoring in big data at UNR. With Dr. Mihye Ahn as my faculty mentor, I am a student researcher funded by the IDeA Network of Biomedical Research Excellence UROP Award. My current work is focused on traumatic brain injury survival classification. Specifically, I use both supervised machine learning and traditional statistical methods to predict the survival of traumatic brain injury patients according to National Trauma Data Bank data. The primary aim of my research is to deploy the best-performing model in a portable and self-contained format to be utilized in healthcare settings to assist professionals with TBI severity assessment and quantitatively informed decision making. I am currently also an honors peer coach, where I help first year honors students gain experience networking with professors while individually mentoring them in their pursuits towards their academic goals. In Spring 2021, I will be working at the Nevada Center for Bioinformatics under Dr. Juli Petereit."),
+                          p("Currently, I am an honors student majoring in mathematics and minoring in big data at UNR. With Dr. Mihye Ahn as my faculty mentor, I am a student researcher funded by the IDeA Network of Biomedical Research Excellence UROP Award. My current work is focused on traumatic brain injury survival classification. Specifically, I use both supervised machine learning and traditional statistical methods to predict the survival of traumatic brain injury patients according to National Trauma Data Bank data. The primary aim of my work is to deploy the best-performing model in a portable and self-contained format to be utilized in healthcare settings to assist professionals with Traumatic Brain Injury severity assessment and quantitatively informed decision making. I am currently also an honors peer coach, where I help first year honors students gain experience networking with professors while individually mentoring them in their pursuits towards their academic goals. In Spring 2021, I will be working at the Nevada Center for Bioinformatics under Dr. Juli Petereit."),
                           
                           h4("My Interests"),   
-                          p("The areas that I am most interested in and have the most experience with are binary classification, dimensionality reduction, and predictive modeling. In addition to mathematics and statistics, I really enjoy learning about biology, especially concerning problems in immunology and bioinformatics. Although study design is a relatively established field, I also find the statistical aspects of studies fascinating and could imagine myself pursuing a graduate degree where both epidemiological concepts and statistical theory are combined. I also quite like programming in relation to high performance computing and web-application development. In my free-time I like working out, programming, and mindfulness meditation."),
+                          p("The areas that I am most interested in and have the most experience with are binary classification, dimensionality reduction, and predictive modeling. In addition to mathematics and statistics, I really enjoy learning about biology, especially concerning problems in immunology and bioinformatics. Although study design is a relatively established field, I also find the statistical aspects of studies fascinating and could imagine myself pursuing a graduate degree where both epidemiological concepts and statistical theory are combined. I also quite like programming in relation to high-performance-computing and web-application development. In my free-time I like working out, mindfulness meditation, and programming/coding."),
                           
                           h4("Website Info"),
-                          p("I created this website as a personal project to improve my HTML and R Shiny programming skills, and recently expanded this website as a portfolio to present my current work and personal projects in a organized manner. Since I am continually working to improve this website, feel free to inform me if something does not work correctly."),
+                          p("I created this website as a personal project to improve my R Shiny and HTML programming skills, and recently expanded this website as a portfolio to present my current work and personal projects in a organized manner. Since I am continually working to improve this website, feel free to inform me if something does not work correctly."),
                                     
                  ), 
                  
@@ -59,12 +59,12 @@ ui <- tagList(
                             tags$hr(),
                             tags$li(
                               h5("Imbalanced Outcome Classification"),
-                              p("Although there currently exist Logistic Regression (LR) based TBI prediction models, these do not account for the fact that the rates of pediatric patient non-survival is much lower than for older TBI patients, and so it follows that the number of observations for the outcome variable are unequally distributed such that there are many more non-cases (survivors) than cases (non-survivors). This imbalance in the outcome variable leads to an overestimation of the probability of the majority class, and more importantly, to an underestimation of the probability of the minority class (as an emergent property). In the context of healthcare, false positives can be costly, but false negatives can be life-threatening, depending on the purpose of the utilized model. Therefore, the methods I utilize include case-weighted models, subsampling techniques, and performance evaluation metrics that account for class imbalance.")
+                              p("Although there currently exist Logistic Regression (LR) based TBI prediction models, these do not account for the fact that the rates of pediatric patient non-survival is much lower than for older TBI patients, and so it follows that the number of observations for the outcome variable are unequally distributed such that there are many more non-cases (survivors) than cases (non-survivors). This imbalance in the outcome variable leads to an overestimation of the probability of the majority class, and more importantly, to an underestimation of the probability of the minority class. In the context of healthcare, false positives can be costly, but false negatives can be life-threatening, depending on the purpose of the utilized model. Therefore, the methods I utilize include case-weighted models, subsampling techniques, and robust performance evaluation metrics.")
                             ),
                             tags$hr(),
                             tags$li(
                               h5("High Performance Computing"),
-                              p("Working with large amounts of obervations and computationally intensive machine learning models leads to model training and hyperparameter tuning times that can range from days to weeks. Thus, I make use of parallelization and High Performance Computing (HPC) capabilities to minimize model fitting time. I run most of my models on the local UNR HPC Clusters Pronghorn and Okapi. Specifically, I deploy models through containerization using both Singularity and Docker. Containerization, in relation to data analysis, can be beneficial, since dependencies such packages, software, and operating systems can be set up in a lightweight manner such that analyses are both reproducible and portable. Singularity is especially useful in my work, since Docker containers require administrator privileges, which is not a viable option for HPC clusters such as Pronghorn.")
+                              p("Working with large amounts of obervations and computationally intensive machine learning models leads to model training and hyperparameter tuning times that can range from days to weeks. Thus, I make use of parallelization and High-Performance-Computing (HPC) capabilities to minimize model fitting times. I run most of my models on the local UNR HPC Clusters Pronghorn and Okapi. Specifically, I deploy models through containerization using both Singularity and Docker. Containerization, in relation to data analysis, can be beneficial, since dependencies such packages, software, and operating systems can be set up in a lightweight manner such that analyses are both reproducible and portable. Singularity is especially useful in my work, since Docker containers require administrator privileges, which is not a viable option for HPC clusters such as Pronghorn.")
                             )
                           )
                  ),
@@ -81,7 +81,7 @@ ui <- tagList(
                           tags$ul(
                             tags$li(
                               tags$b("(R-Based) Project Introduction"),
-                              p("The first aim of this stochastic models and simulation project is to understand the metropolis-hastings algorithm and several markov-chain-monte-carlo diagnostic methods at a more intuitive and visual level through plots that are both animated and interactive. My second aim is to present my first aim in a cohesive and compect manner to those unfamiliar with MCMC and the R-Programming Language. It is important to mention that the time-dependent nature of a markov chain and the amount of visually appealing parameters are optimal for creating animated illustrations. The next step I will take for this project is to add a tab that brings all other concepts together in one cohesive analysis.")
+                              p("The first aim of this stochastic models and simulation project is to understand the metropolis-hastings algorithm and several markov-chain-monte-carlo diagnostic methods at a more intuitive and visual level through plots that are both animated and interactive. My second aim is to present my first aim in a cohesive and compact manner to those unfamiliar with MCMC and the R-Programming Language. It is important to mention that the time-dependent nature of a markov chain and the amount of visually appealing parameters are optimal for creating animated illustrations. The next step I will take for this project is to add a tab that brings all other concepts together in one cohesive analysis.")
                             )
                           ),
                           tags$br(),
@@ -91,15 +91,6 @@ ui <- tagList(
                             tags$li(
                               tags$b("(R-Based) Project Introduction"),
                               p("The aims of this project are to simulate multivariate data from three underlying linear models with varying degrees of correlation among predictors and then observing how effective different variable selection methods perform on each dataset by fitting a linear regression model with the selected variables. Thus, we first simulate 50 datasets for every underlying model from section 7 of the Paper \"Regression Shrinkage and Selection via the Lasso\" by Tibshirani (1996). We then fit the models according several variable selection and shrinkage methods and consider Akaike Information Criterion (AIC), Bayesian Information Criterion (BIC), and Adjusted R-Squared Value as performance metrics. My next goal is to vectorize all for loops and generally reduce redundant variable storage where possible.")
-                            ),
-                          ),
-                          tags$br(),
-                          
-                          h5("Web Mining and Task Automation Toolset"),
-                          tags$ul(
-                            tags$li(
-                              tags$b("(Python-Based) Project Introduction"),
-                              p("Work in Progress.")
                             ),
                           ),
                           tags$br(),
@@ -117,6 +108,15 @@ ui <- tagList(
                           tags$ul(
                             tags$li(
                               tags$b("(R-Based) Project Introduction"),
+                              p("Work in Progress.")
+                            ),
+                          ),
+                          tags$br(),
+                          
+                          h5("Web Mining and Task Automation Toolset"),
+                          tags$ul(
+                            tags$li(
+                              tags$b("(Python-Based) Project Introduction"),
                               p("Work in Progress.")
                             ),
                           ),
